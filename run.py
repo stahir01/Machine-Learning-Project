cca98227eb02b1b290ba49bae7b1982564778934
pml_12 <pml_12@node04.ml.tu-berlin.de>
2022-11-07 20:01:10 +0100

    [ADD] Training

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from model import *
 
 import torch
 
-def train(epoch, batch_size=5, learning_rate=1e-3):
+def train(epochs, batch_size=5, learning_rate=1e-3):
     dataset = ISBIEMSegDataset('./data/isbi_em_seg', transform=Compose([ToTensor(), Resize((572, 572))]))
     dataloader = DataLoader(dataset, batch_size=batch_size)
 
@@ -12,20 +12,22 @@ def train(epoch, batch_size=5, learning_rate=1e-3):
 
     criterium = torch.nn.BCELoss()
     optimizer = torch.optim.Adam(lr=learning_rate, params=model.parameters())
+    for epoch in epochs:
+        for batch_index, (image, label) in enumerate(dataloader):
+            model.train()
 
-    for batch_index, (image, label) in enumerate(dataloader):
-        image, label = image.to(device), label.to(device)
-    
-        optimizer.zero_grad()
-        output = model(image)
+            image, label = image.to(device), label.to(device)
+        
+            optimizer.zero_grad()
+            output = model(image)
 
-        loss = criterium(output, label)
-        loss.backward()
+            loss = criterium(output, label)
+            loss.backward()
 
-        optimizer.step()
+            optimizer.step()
 
-        if batch_index % 10 == 0:
-            print("Training Epoch: {} [{}/{} ({:.0f}%) \t Loss:{:.6f}]".format(epoch, batch_index*len(image), len(dataloader), 100. * batch_index/len(dataloader), loss.item()), end='\r')
+            if batch_index % 10 == 0:
+                print("Training Epoch: {} [{}/{} ({:.0f}%) \t Loss:{:.6f}]".format(epoch, batch_index*len(image), len(dataloader), 100. * batch_index/len(dataloader), loss.item()), end='\r')
 
 if __name__ == "__main__":
     
