01e3c1a7dffd66be5308cefbe5349f165e90e9c8
Ali Murad <alimurad00@hotmail.com>
2023-02-01 17:07:04 +0100

    Calculated Confidence score and added prediction function

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import time
+import numpy as np
 
 from data_loading import load_data
 from model import NewUNet
@@ -21,108 +22,52 @@ import matplotlib.pyplot as plt
 from metrics import calculate_iou
 
 def get_test_metrics(model, test_loader, criterion, device):
-   losses, ious = [], []
+   losses, ious, confidences = [], [], []
    with torch.no_grad():
       for i, data in enumerate(test_loader):
 
          image, mask = data[0].to(device), data[1].to(device)
          output = model(image)
          output = output[:,1].unsqueeze(dim=1)
-
+         
          loss = criterion(output, mask)
          losses.append(loss.item())
 
          iou = calculate_iou(output, mask)
          ious.append(iou.item())
 
-   return torch.tensor(losses).mean(), torch.tensor(ious).mean()
+   return torch.tensor(losses).mean().cpu().numpy().astype(float), torch.tensor(ious).mean().cpu().numpy().astype(float), torch.tensor(confidences).mean().cpu().numpy().astype(float)
 
 def custom_loss(output, mask):
-   # return (output - mask).abs().mean(dim=(1,2,3)).mean()
-   return ((output - mask)**2).mean(dim=(1,2,3)).norm()
+   return (output - mask).abs().mean(dim=(1,2,3)).mean()
+   # return ((output - mask)**2).mean(dim=(1,2,3)).norm()
 
-def test_model(model, test_loader, criterion, device):
-    
-    model = model.eval()
-    predictions, mask_result = [], []
-    
-    with torch.no_grad():
+def test_model(model, test_loader, device):
+   model.eval()
+
+   predictions, confidences = [], []
+   with torch.no_grad():
       for i, data in enumerate(tqdm(test_loader)):
          image, mask = data[0].to(device), data[1].to(device)
          output = model(image)
+
+         probabilities = torch.softmax(output, dim=1)
          
-         test_output = model(image)  
-         test_output = test_output[:,1].unsqueeze(dim=1)  
-
-         #Test score       
-         test_score = calculate_iou_batch(test_output, mask)
-         avg_test_score = (torch.tensor(test_score).mean()).cpu().numpy().astype(float)
-
-         #Pixel accuracy
-         pixel_score = pixel_accuracy(test_output, mask)
-         avg_pixel_score = (torch.tensor(pixel_score).mean()).cpu().numpy().astype(float)
-
-         #Dice Score
-         dice_score = dice_coeff(test_output, mask)
-         avg_dice_score = (torch.tensor(dice_score).mean()).cpu().numpy().astype(float)
+         #Calculate confidence score
+         confidence, index = torch.max(probabilities, dim=1)
+         confidence_mean = torch.mean(confidence).item()
 
 
+         confidences.append(confidence_mean)
          predictions.append(output)
-         mask_result.append(mask) #Used for plotting images labels/masks
-
          
-    predictions = torch.cat(predictions, dim=0)
-    mask = torch.cat(mask_result, dim=0)
-    return predictions, mask, avg_test_score, avg_pixel_score, avg_dice_score
-
-def train_model(model, train_loader, optimizer, criterion, device, num_epoch = 100):
-  
-  avg_loss_val, avg_score_val = [], []
-  model.train()
-
-  for i in range (1, num_epoch+1):
-    
-    losses = []
-    
-    for batch_index, (image, label) in enumerate(tqdm(train_loader)):
-      image, mask = image.to(device), label.to(device)
-      
-      optimizer.zero_grad()
-      output = model(image)
-      output = output[:,1].unsqueeze(dim=1)
-      
-      loss = criterion(output, mask)
-      score = calculate_iou_batch(output, label)
-      losses.append(loss)
-      loss.backward()
-      optimizer.step()
-       
-      avg_loss = torch.tensor(losses).mean()
-      avg_score = torch.tensor(score).mean()
-
-    avg_loss_val.append(avg_loss)
-    avg_score_val.append(avg_score)
-    print("Epoch {0}: train_loss {1} \t train_score {2}".format(i, avg_loss, avg_score))
-  plt.figure(figsize=(25,5))
-  plt.plot(avg_loss_val,'-o')
-  plt.plot(avg_score_val,'-x')
-  plt.xlabel('epochs')
-  plt.ylabel('Avg Loss & Score')
-  plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
-  plt.legend(['Train Loss', 'Train Score'])
-  plt.title('Train Loss & Train Score')
-  plt.show()
-  plt.pause(5)
-  plt.close()
 
 
+   predictions = torch.cat(predictions, dim=0)
+   confidences = np.array(confidences)
 
+   return predictions, mask, confidences.mean()
 
-
-
-
-
-'''
 def train_one_epoch(model, train_loader, optimizer, criterion, device):
    losses, ious = [], []
    for i, (image, mask) in enumerate(tqdm(train_loader, leave=False)):
@@ -140,8 +85,8 @@ def train_one_epoch(model, train_loader, optimizer, criterion, device):
       loss.backward()
       optimizer.step()
 
-   avg_loss = torch.tensor(losses).mean()
-   avg_iou = torch.tensor(ious).mean()
+   avg_loss = torch.tensor(losses).mean().cpu().numpy().astype(float)
+   avg_iou = torch.tensor(ious).mean().cpu().numpy().astype(float)
    return avg_loss, avg_iou
          
 def train_model(model, train_loader, test_loader, optimizer, criterion, device, num_epoch = 100):
@@ -152,28 +97,23 @@ def train_model(model, train_loader, test_loader, optimizer, criterion, device, 
 
    for epoch in range(num_epoch): 
       avg_loss_train, avg_iou_train = train_one_epoch(model, train_loader, optimizer, criterion, device)
-      avg_loss_test, avg_iou_test = get_test_metrics(model, test_loader, criterion, device)
+      avg_loss_test, avg_iou_test, prec_score = get_test_metrics(model, test_loader, criterion, device)
 
       avg_ious_train.append(avg_iou_train)
       avg_ious_test.append(avg_iou_test)
       avg_losses_train.append(avg_loss_train)
       avg_losses_test.append(avg_loss_test)
-      print(epoch, avg_loss_train, avg_iou_train, avg_loss_test, avg_iou_test)
-
+      print("Epoch {0}: train_loss {1} \t train_score {2} \t test_loss {3} \t test_score{4} \t precision_score {5}".format(epoch, avg_loss_train, avg_iou_train, avg_loss_test, avg_iou_test, prec_score))
+      
+   plt.figure(figsize=(25,5))
    plt.plot(avg_losses_train, label='Training Loss')
    plt.plot(avg_ious_train, label='Training IoU')
    plt.plot(avg_losses_test, label='Testing Loss')
    plt.plot(avg_ious_test, label='Testing IoU')
+   plt.xlabel('epochs')
+   plt.ylabel('Avg Loss & Score')
+   plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+   plt.title('Train Loss & Train Score')
    plt.legend(loc='best')
    plt.grid(visible=True, which='both')
    plt.show()
-
-def main():
-   pass
-
-   # torch.save(model.state_dict(), 'models/model_' + SUFFIX + '.pth')
-   # print('models/model_' + SUFFIX + '.pth')
-
-if __name__ == "__main__":
-   main()
-'''
--- train_apply.py
+++ train_apply.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn, optim
+import numpy as np
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 from data_loading import load_data
@@ -8,69 +10,92 @@ from model import NewUNet
 from train import train_model, test_model
 from entropy_loss import DiceBCELoss
 
-NUM_EPOCHS = 25
-LR = 0.01
-MOMENTUM = .99
-BATCH_SIZE = 1
+#NUM_EPOCHS = 50
+#LR = 0.07
+#MOMENTUM = 99
+#BATCH_SIZE = 2
 
-SUFFIX = f'EP_{NUM_EPOCHS}_LR_{LR}_MOM_{MOMENTUM}_BS_{BATCH_SIZE}'
+#SUFFIX = f'EP_{NUM_EPOCHS}_LR_{LR}_MOM_{MOMENTUM}_BS_{BATCH_SIZE}'
 
-def train_apply(method = 'train_model',dataset = 'isbi_em_seg', num_epochs=25, lr=0.01, momentum=0.99, batch_size=3, in_channel = 1):
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        bce_weight = 0.5
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        loss_final = BCE * bce_weight + dice_loss * (1 - bce_weight)
+        return loss_final
+
+def train_apply(method = 'train_model',dataset = 'isbi_em_seg', num_epochs=25, lr=0.01, momentum=0.99, batch_size=3):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f"Running on: {device}")
 
-    train_loader, test_loader = load_data(dataset=dataset, batch_size=batch_size, n_train=0.8, n_test=0.2)
-    
-    # switch to RGB dataset
-    if dataset != 'isbi_em_seg' and dataset != 'isbi_em_seg_100':
-        in_channel = 3
-
-    model = NewUNet(in_channel).to(device)
+    train_loader, test_loader = load_data(dataset=dataset, batch_size=batch_size)
+    model = NewUNet(1).to(device)
 
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)   
-    #criterion = nn.BCEWithLogitsLoss()
     criterion = DiceBCELoss()
 
     eval(f'{method}(model, train_loader, test_loader, optimizer, criterion, device, num_epoch={num_epochs})')
 
-    predictions, mask, avg_test_score, avg_pixel_score, avg_dice_score = test_model(model, test_loader, criterion, device)
+    predictions, mask, confidence_score = test_model(model, test_loader, device)
 
-    return predictions
+    return predictions, mask, confidence_score
 
-def main():
-    predictions = train_apply(num_epochs=50)
-    print(predictions.shape)
-
-if __name__ == '__main__':
-    predictions, mask, test_score, pixel_score, dice_score = train_apply(num_epochs=NUM_EPOCHS, lr=LR, batch_size=BATCH_SIZE, dataset='isbi_em_seg')
-    #print(predictions.shape)
+def predict(learning_rate=0.01, epochs=25, batch_size=3, momentum=0.99):
+    predictions, mask, confidence_score = train_apply(lr=learning_rate, num_epochs=epochs, batch_size=batch_size, momentum=momentum)
+    print("Confidence score: ", confidence_score)
 
     prediction_test = predictions#[:, 1:2, :, :]
     mask_test = mask
+
+
     #Final Result
     fig = plt.figure(figsize=(20,20))
     for i in range (len(prediction_test)):
-        while i < 5:
-            predict_results = prediction_test[i].cpu().numpy()
-            mask_results = mask_test[i].cpu().numpy()
+      while i < 2:
+
+        predict_results = prediction_test[i].cpu().numpy()
+        mask_results = mask_test[i].cpu().numpy()
 
 
-            #predict_results = (predict_results * 255.0).astype("uint8")
-            #mask_results = (mask_results * 255.0).astype("uint8")
+        #predict_results = (predict_results * 255.0).astype("uint8")
+        #mask_results = (mask_results * 255.0).astype("uint8")
 
-            predict_results = predict_results[0]
-            mask_results = mask_results[0]
+        predict_results = predict_results[0]
+        mask_results = mask_results[0]
 
-            predict_results = predict_results.squeeze()
-            mask_results = mask_results.squeeze()
-            #print("Predict Results: ", predict_results)
-            #print("Mask Results: ", mask_results)
+        predict_results = predict_results.squeeze()
+        mask_results = mask_results.squeeze()
+        #print("Predict Results: ", predict_results)
+        #print("Mask Results: ", mask_results)
 
-            plt.subplot(5,2,2*i+1)
-            plt.imshow(mask_results)
-            plt.axis("off")
-            plt.subplot(5,2,2*i+2)
-            plt.imshow(predict_results, cmap = 'gray') 
-            plt.axis("off")
-            i+=1
+        plt.subplot(5,2,2*i+1)
+        plt.imshow(mask_results, cmap = 'gray')
+        plt.axis("off")
+        plt.subplot(5,2,2*i+2)
+        plt.imshow(predict_results, cmap = 'gray') 
+        plt.axis("off")
+        i+=1
     plt.show()
+
+
+
+
+if __name__ == '__main__':
+    NUM_EPOCHS = 1
+    LR = 0.07
+    MOMENTUM = 0.99
+    BATCH_SIZE = 2
+
+    predict(learning_rate=LR, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, momentum=MOMENTUM)
